2b2b0902ba1b8b51030ccafcda68c8cdce0f6320
Lenddy <leme2003@gmail.com>
2023-05-22 11:08:19 -0400

    orders

---
+++ models/Orders.js
@@ -0,0 +1,19 @@
+import { Schema, model, models } from "mongoose";
+
+const OrderSchema = new Schema(
+	{
+		line_items: Object,
+		name: String,
+		email: String,
+		city: String,
+		postalCode: String,
+		streetAddress: String,
+		country: String,
+		paid: Boolean,
+	},
+	{ timestamps: true }
+);
+
+export const Order = models?.Order || model("Order", OrderSchema);
+
+// module.exports = Order;
---
+++ pages/orders.jsx
@@ -0,0 +1,13 @@
+import Layout from "@/components/Layout";
+import React from "react";
+
+const OrdersPage = () => {
+	return (
+		<Layout>
+			<h1>Orders</h1>
+			<table className="basic"></table>
+		</Layout>
+	);
+};
+
+export default OrdersPage;
