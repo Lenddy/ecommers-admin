c13c7b1b4ac15b2fb02cf03981c3d20827057ec7
Lenddy Morales <leme2003@gmail.com>
2023-05-24 13:45:47 -0400

    orders

--- pages/orders.jsx
+++ pages/orders.jsx
@@ -1,12 +1,42 @@
 import Layout from "@/components/Layout";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const OrdersPage = () => {
+	const [orders, setOrders] = useState([]);
+	useEffect(() => {
+		axios.get("/api/orders").then((res) => {
+			setOrders(res.data);
+		});
+	}, []);
 	return (
 		<Layout>
 			<h1>Orders</h1>
-			<table className="basic"></table>
-			<h1>hello</h1>
+			<table className="basic">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Recipient</th>
+						<th>Products</th>
+					</tr>
+					<tbody>
+						{orders.length > 0 &&
+							orders.map((order) => (
+								<tr>
+									<td>{order._id}</td>
+									<td>
+										{order.name} {order.email} <br />
+										<br />
+										{order.city} {order.postalCode}{" "}
+										{order.country} <br />
+										{order.streetAddress}
+									</td>
+									<td>hello</td>
+								</tr>
+							))}
+					</tbody>
+				</thead>
+			</table>
 		</Layout>
 	);
 };
